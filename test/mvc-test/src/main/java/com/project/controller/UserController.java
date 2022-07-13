9c4238b97b305a6d21e010b6186eeae80f160d94
fanxing056 <1192761471@qq.com>
2022-07-13 16:01:31 +0800

    16:01

--- test/mvc-test/src/main/java/com/project/controller/UserController.java
+++ test/mvc-test/src/main/java/com/project/controller/UserController.java
@@ -19,13 +19,13 @@ public class UserController {
     /***
      * @auther shaohuina
      * @param map
-     * @return:返回用户名、密码
+     * @return
      */
     @RequestMapping("login")
     public BaseRespVO login(@RequestBody Map map){
         String username = (String) map.get("username");
         String password = (String) map.get("password");
-        return BaseRespVO.ok("hello, "+username + "--" + password);
+        return BaseRespVO.ok("goodbye" + username + "--" + password);
     }
 
     /**
@@ -41,8 +41,8 @@ public class UserController {
     /**
      * xrw
      */
-    @RequestMapping("login666")
-    public BaseRespVO login666(String username) {
+    @RequestMapping("login6666")
+    public BaseRespVO login6666(String username) {
         return BaseRespVO.ok(username);
     }
 
