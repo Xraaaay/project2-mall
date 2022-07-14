a0aa4523cfdc065431a78403627388110ba1a08f
Xraaaay <xiaorw1227@gmail.com>
2022-07-14 20:50:02 +0800

    Auther:xrw Date:20220714 Note:搭建SpringBoot应用

--- mall/src/main/java/com/cskaoyan/bean/BaseRespVO.java
+++ mall/src/main/java/com/cskaoyan/bean/BaseRespVO.java
@@ -1,23 +1,28 @@
-package com.project.bean;
+package com.cskaoyan.bean;
 
 import lombok.Data;
 
+/**
+ * @Description
+ * @Author Xrw
+ * @Date 2022/7/14 19:33
+ */
 @Data
 public class BaseRespVO<T> {
     private T data;
     private Integer code;
     private String msg;
 
-    public static BaseRespVO ok() {
-        return ok(null);
-    }
-
-    public static <T> BaseRespVO<T> ok(T data) {
+    public static <T> BaseRespVO ok(T data) {
         BaseRespVO<T> baseRespVO = new BaseRespVO<>();
         baseRespVO.setCode(200);
         baseRespVO.setMsg("OK");
         baseRespVO.setData(data);
         return baseRespVO;
+    }
+
+    public static BaseRespVO ok() {
+        return ok(null);
     }
 
     public static BaseRespVO error(String msg) {
---
+++ mall/src/main/java/com/cskaoyan/config/MvcConfiguration.java
@@ -0,0 +1,14 @@
+package com.cskaoyan.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @Description
+ * @Author Xrw
+ * @Date 2022/7/14 20:03
+ */
+@Configuration
+public class MvcConfiguration implements WebMvcConfigurer {
+
+}
