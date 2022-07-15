eabdf65f42c0b1be81fc9c089244d680610f8afc
Xraaaay <xiaorw1227@gmail.com>
2022-07-15 17:38:28 +0800

    Author:xrw Note:搭建springboot应用 Date:2022-07-15

--- mall/src/main/java/com/cskaoyan/MarketApplication.java
+++ mall/src/main/java/com/cskaoyan/MarketApplication.java
@@ -5,11 +5,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.cskaoyan.mapper")
-public class Application {
+// @MapperScan("com.cskaoyan.mapper")
+public class MarketApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(MarketApplication.class, args);
     }
 
 }
--- mall/src/main/java/com/cskaoyan/bean/BaseRespVO.java
+++ mall/src/main/java/com/cskaoyan/bean/BaseRespVO.java
@@ -13,22 +13,5 @@ public class BaseRespVO<T> {
     private Integer code;
     private String msg;
 
-    public static <T> BaseRespVO ok(T data) {
-        BaseRespVO<T> baseRespVO = new BaseRespVO<>();
-        baseRespVO.setCode(200);
-        baseRespVO.setMsg("OK");
-        baseRespVO.setData(data);
-        return baseRespVO;
-    }
 
-    public static BaseRespVO ok() {
-        return ok(null);
-    }
-
-    public static BaseRespVO error(String msg) {
-        BaseRespVO baseRespVO = new BaseRespVO();
-        baseRespVO.setCode(500);
-        baseRespVO.setMsg(msg);
-        return baseRespVO;
-    }
 }
