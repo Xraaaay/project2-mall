95be91850bc1ea816ef38234c548ea9a40e3a0c7
fanxing056 <1192761471@qq.com>
2022-07-17 15:00:45 +0800

    Author:fanxing056 Note: 阿里云组件 Date: 2022-07-17

---
+++ mall/src/main/java/com/cskaoyan/config/aliyun/Oss.java
@@ -0,0 +1,16 @@
+package com.cskaoyan.config.aliyun;
+
+import lombok.Data;
+
+/**
+ * Oss参数类
+ * @date 2022/07/17 14:47
+ * @author fanxing056
+ */
+
+@Data
+public class Oss {
+
+    String bucket;
+    String endPoint;
+}
