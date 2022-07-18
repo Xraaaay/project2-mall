a131d905e1cd0db4b7f44daef64ff5ece769be41
zhaoahui <3236795920@qq.com>
2022-07-18 08:34:22 +0800

    author:Zah Note:修改冲突，合并 Date:2022-07-18

--- mall/src/main/java/com/cskaoyan/controller/dashboard/DashBoardController.java
+++ mall/src/main/java/com/cskaoyan/controller/dashboard/DashBoardController.java
@@ -1,6 +1,8 @@
 package com.cskaoyan.controller.dashboard;
 
 import com.cskaoyan.bean.common.BaseRespVo;
+import com.cskaoyan.bean.dashboard.DashBoardVo;
+import com.cskaoyan.service.dashboard.DashBoardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,10 +17,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class DashBoardController {
 
     @Autowired
+    DashBoardService dashBoardService;
 
 
     @RequestMapping("admin/dashboard")
     public BaseRespVo dashBoard(){
-        return null;
+        DashBoardVo dashBoardVo = dashBoardService.dashBoard();
+        return BaseRespVo.ok(dashBoardVo);
     }
 }
