b26ef4a5ba883c9df172b7b2dacdd10ca33351fe
zhaoahui <3236795920@qq.com>
2022-07-18 22:14:42 +0800

    author:Zah Note:完成用户管理 Date:2022-07-18

--- mall/src/main/java/com/cskaoyan/controller/adminauth/AuthController.java
+++ mall/src/main/java/com/cskaoyan/controller/adminauth/AuthController.java
@@ -1,21 +1,20 @@
 package com.cskaoyan.controller.adminauth;
 
-import com.cskaoyan.bean.adminauth.AdminInfoBean;
+
 import com.cskaoyan.bean.adminauth.InfoData;
 import com.cskaoyan.bean.adminauth.LoginUserData;
 import com.cskaoyan.bean.common.BaseRespVo;
-import com.cskaoyan.bean.system.MarketAdmin;
+import com.cskaoyan.service.adminauth.AdminLoginService;
 import com.cskaoyan.util.Md5Utils;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.subject.Subject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.Map;
 
 //shiro整合之后，在做具体的开发
@@ -24,8 +23,12 @@ import java.util.Map;
 @RequestMapping("admin/auth")
 public class AuthController {
 
+    @Autowired
+    AdminLoginService adminLoginService;
+
     /**
      * 整合Shiro之后的login
+     *
      * @param map
      * @return com.cskaoyan.bean.common.BaseRespVo<com.cskaoyan.bean.adminauth.LoginUserData>
      * @author Zah
@@ -34,37 +37,56 @@ public class AuthController {
     @PostMapping("login")
     public BaseRespVo<LoginUserData> login(@RequestBody Map map) throws Exception {
 
-        String username = (String)map.get("username");
-        String originalPassword = (String)map.get("password");
+        String username = (String) map.get("username");
+        String originalPassword = (String) map.get("password");
 
         // 将密码通过MD5进行加密
         String password = Md5Utils.getMd5(originalPassword);
 
-        // 认证登录准备
-        Subject subject = SecurityUtils.getSubject();
         UsernamePasswordToken token = new UsernamePasswordToken(username, password);
-        // 认证登录
-        subject.login(token);
 
-        boolean authenticated = subject.isAuthenticated();
+        LoginUserData loginUserData = adminLoginService.adminLogin(token);
 
-        // 获取经过认证之后的用户信息
-        MarketAdmin primaryPrincipal = (MarketAdmin) subject.getPrincipals().getPrimaryPrincipal();
-        // 获取SessionId
-        Serializable sessionId = subject.getSession().getId();
-
-        LoginUserData loginUserData = new LoginUserData();
-        AdminInfoBean adminInfo = new AdminInfoBean();
-        adminInfo.setAvatar(primaryPrincipal.getAvatar());
-        adminInfo.setNickName(primaryPrincipal.getUsername());
-        loginUserData.setAdminInfo(adminInfo);
-        loginUserData.setToken((String) sessionId);
         return BaseRespVo.ok(loginUserData);
     }
 
+    /**
+     * 经登录认证之后的管理员信息
+     *
+     * @param token
+     * @return com.cskaoyan.bean.common.BaseRespVo
+     * @author Zah
+     * @date 2022/07/18 14:22
+     */
+    @RequestMapping("info")
+    public BaseRespVo info(String token) {
+
+        InfoData infoData = adminLoginService.adminInfo();
+
+        if (infoData == null) {
+            return BaseRespVo.invalidData("未登录，请登录之后在操作");
+        }
+
+        return BaseRespVo.ok(infoData);
+    }
+
+    /**
+     * 登出
+     *
+     * @return com.cskaoyan.bean.common.BaseRespVo
+     * @author Zah
+     * @date 2022/07/18 14:23
+     */
+    @PostMapping("logout")
+    public BaseRespVo logout() {
+        Subject subject = SecurityUtils.getSubject();
+        subject.logout();
+        return BaseRespVo.ok(null);
+    }
+
     @RequestMapping("401")
-    public BaseRespVo unAuthc(){
-        return new BaseRespVo(null,501,"请登录");
+    public BaseRespVo unAuthc() {
+        return new BaseRespVo(null, 501, "请登录");
     }
 
     @RequestMapping("403")
@@ -76,34 +98,5 @@ public class AuthController {
     public BaseRespVo index() {
         return BaseRespVo.ok(null);
     }
-
-    @PostMapping("logout")
-    public BaseRespVo logout(){
-        Subject subject = SecurityUtils.getSubject();
-        subject.logout();
-        return BaseRespVo.ok(null);
-    }
-
-    @RequestMapping("info")
-    public BaseRespVo info(String token) {
-
-
-        //开发完shiro之后，再整合
-        InfoData infoData = new InfoData();
-        infoData.setName("admin123");
-
-        //根据primaryPrincipal做查询
-        infoData.setAvatar("https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif");
-        ArrayList<String> roles = new ArrayList<>();
-        roles.add("超级管理员");
-        infoData.setRoles(roles);
-        ArrayList<String> perms = new ArrayList<>();
-        perms.add("*");
-        infoData.setPerms(perms);
-
-
-        return BaseRespVo.ok(infoData);
-    }
-
 
 }
---
+++ mall/src/main/java/com/cskaoyan/controller/usermanagement/UserController.java
@@ -0,0 +1,41 @@
+package com.cskaoyan.controller.usermanagement;
+
+import com.cskaoyan.bean.MarketUser;
+import com.cskaoyan.bean.common.BaseParam;
+import com.cskaoyan.bean.common.BaseRespVo;
+import com.cskaoyan.bean.usermanagement.UserListVo;
+import com.cskaoyan.service.adminusermanagement.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 用户管理之会员管理
+ *
+ * @author Zah
+ * @date 2022/07/18 17:36
+ */
+@RestController
+@RequestMapping("admin/user")
+public class UserController {
+
+    @Autowired
+    UserService userService;
+
+    @RequestMapping("list")
+    public BaseRespVo list(BaseParam page,String username,String mobile){
+
+        UserListVo userList = userService.getUserList(page, username, mobile);
+
+        return BaseRespVo.ok(userList);
+    }
+
+
+    @RequestMapping("detail")
+    public BaseRespVo detail(Integer id){
+
+        MarketUser userDetail = userService.getUserDetail(id);
+
+        return BaseRespVo.ok(userDetail);
+    }
+}
