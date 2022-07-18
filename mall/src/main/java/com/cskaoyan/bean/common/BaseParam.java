182738f57b550421564c8f0e2035b0b5584bb760
Xraaaay <xiaorw1227@gmail.com>
2022-07-18 23:54:17 +0800

    Author:xrw Note:授权 Date:2022-07-18

--- mall/src/main/java/com/cskaoyan/bean/common/BaseParam.java
+++ mall/src/main/java/com/cskaoyan/bean/common/BaseParam.java
@@ -3,11 +3,9 @@ package com.cskaoyan.bean.common;
 import lombok.Data;
 
 /**
- * 已过时，使用com.cskaoyan.bean.common.BasePageInfo
  * @author stone
  * @date 2022/01/06 16:37
  */
-@Deprecated
 @Data
 public class BaseParam {
 
--- mall/src/main/java/com/cskaoyan/controller/adminauth/AuthController.java
+++ mall/src/main/java/com/cskaoyan/controller/adminauth/AuthController.java
@@ -1,7 +1,6 @@
 package com.cskaoyan.controller.adminauth;
 
 import com.cskaoyan.anno.OperationLog;
-import com.cskaoyan.bean.adminauth.AdminInfoBean;
 import com.cskaoyan.bean.adminauth.InfoData;
 import com.cskaoyan.bean.adminauth.LoginUserData;
 import com.cskaoyan.bean.common.BaseRespVo;
@@ -17,8 +16,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.Map;
 
 //shiro整合之后，在做具体的开发
@@ -86,7 +83,7 @@ public class AuthController {
      */
 
     // AOP 安全操作日志xrw
-    @SecurityOperationLog(SecurityOperationType.LOGOUT)
+    @OperationLog(action = "退出")
     @PostMapping("logout")
     public BaseRespVo logout() {
         Subject subject = SecurityUtils.getSubject();
@@ -108,36 +105,5 @@ public class AuthController {
     public BaseRespVo index() {
         return BaseRespVo.ok(null);
     }
-
-    // AOP 安全操作日志xrw
-    @OperationLog(action = "退出")
-    @PostMapping("logout")
-    public BaseRespVo logout(){
-        Subject subject = SecurityUtils.getSubject();
-        subject.logout();
-        return BaseRespVo.ok(null);
-    }
-
-    @RequestMapping("info")
-    public BaseRespVo info(String token) {
-
-
-        //开发完shiro之后，再整合
-        InfoData infoData = new InfoData();
-        infoData.setName("admin123");
-
-        //根据primaryPrincipal做查询
-        infoData.setAvatar("https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif");
-        ArrayList<String> roles = new ArrayList<>();
-        roles.add("超级管理员");
-        infoData.setRoles(roles);
-        ArrayList<String> perms = new ArrayList<>();
-        perms.add("*");
-        infoData.setPerms(perms);
-
-
-        return BaseRespVo.ok(infoData);
-    }
-
 
 }
