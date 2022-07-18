1582b577515ec4ea2d3e5f29adaf027ab3b96b74
fanxing056 <1192761471@qq.com>
2022-07-18 10:23:38 +0800

    Author:fanxing056 Note:参数校验工具类 Date:2022-07-18

---
+++ mall/src/main/java/com/cskaoyan/exception/common/InvalidDataException.java
@@ -0,0 +1,17 @@
+package com.cskaoyan.exception.common;
+
+/**
+ * 非法数据异常
+ *
+ * @author fanxing056
+ * @date 2022/07/18 10:12
+ */
+
+public class InvalidDataException extends RuntimeException {
+    public InvalidDataException() {
+    }
+
+    public InvalidDataException(String message) {
+        super(message);
+    }
+}
